84b6dfcfda6f5467cedfc81bec4a1f857f291dea
ranqiwei <1056782369@qq.com>
2018-09-03 16:32:19 +0800

    修复BaseModel

--- model/model.go
+++ model/model.go
@@ -9,7 +9,7 @@ type BaseModel struct {
 	Id        uint64    `gorm:"primary_key;AUTO_INCREMENT;column:id" json:"-"`
 	CreatedAt time.Time `gorm:"column:createdAt" json:"-"`
 	UpdatedAt time.Time `gorm:"column:updatedAt" json:"-"`
-	DeletedAt time.Time `gorm:"column:deletedAt" json:"-"`
+	DeletedAt time.Time `gorm:"column:deletedAt" sql:"index" json:"-"`
 }
 
 type UserInfo struct {

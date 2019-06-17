38da23745467ee03bc8f6e774b0a8fa21927ce41
ranqiwei <1056782369@qq.com>
2019-06-17 14:36:46 +0800

    fixed unit test

--- util/util_test.go
+++ util/util_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGenShortId(t *testing.T) {
 	shortId, err := GenShortId()
-	if shortId == "" || err == nil {
+	if shortId == "" || err != nil {
 		t.Error("GenShortId failed!")
 	}
 	t.Log("GenShortId test pass")

f1fec2128354086140cca42baf4a0cbc97e247f6
ranqiwei <1056782369@qq.com>
2019-06-17 15:04:41 +0800

    add benchmark test

--- util/util_test.go
+++ util/util_test.go
@@ -9,3 +9,30 @@ func TestGenShortId(t *testing.T) {
 	}
 	t.Log("GenShortId test pass")
 }
+
+func BenchmarkGenShortId(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := GenShortId()
+		if err != nil {
+			break
+		}
+	}
+}
+
+func BenchmarkGenShortIdTimeConsuming(b *testing.B) {
+	b.StopTimer() //调用该函数停止压力测试的时间计数
+
+	shortId, err := GenShortId()
+	if shortId == "" || err != nil {
+		b.Error(err)
+	}
+
+	b.StartTimer() //重新开始时间
+
+	for i := 0; i < b.N; i++ {
+		_, err := GenShortId()
+		if err != nil {
+			break
+		}
+	}
+}
